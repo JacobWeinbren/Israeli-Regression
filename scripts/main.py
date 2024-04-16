a92f204cb848d1bb759fe1625c1ac35d4f86eaa5
Jacob Weinbren <jacobweinbren@gmail.com>
2024-04-16 20:46:44 +0100

    Changed to BayesSearch

--- scripts/main.py
+++ scripts/main.py
@@ -1,28 +1,30 @@
 # python scripts/main.py > output/output.log
 
-from sklearn.pipeline import Pipeline
-from imblearn.pipeline import Pipeline as ImbPipeline
-from sklearn.impute import KNNImputer
+import numpy as np
+import pandas as pd
+import pyreadstat
+import joblib
+import logging
+from sklearn.model_selection import (
+    train_test_split,
+    StratifiedKFold,
+)
+from sklearn.metrics import roc_auc_score, make_scorer, classification_report
 from sklearn.preprocessing import (
     StandardScaler,
     PolynomialFeatures,
-    LabelEncoder,
     OneHotEncoder,
+    LabelEncoder,
 )
 from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from imblearn.pipeline import Pipeline as ImbPipeline
+from sklearn.impute import KNNImputer
 from xgboost import XGBClassifier
-from sklearn.model_selection import (
-    train_test_split,
-    RandomizedSearchCV,
-    StratifiedKFold,
-)
-from sklearn.metrics import roc_auc_score, make_scorer
-import logging
-import numpy as np
-import pyreadstat
-import joblib
-import pandas as pd
 from imblearn.over_sampling import BorderlineSMOTE
+from sklearn.calibration import CalibratedClassifierCV
+from skopt import BayesSearchCV
+from skopt.space import Real, Integer
 
 # Custom logging setup
 logging.basicConfig(
@@ -98,12 +100,20 @@ def load_and_prepare_data(filepath):
     return X, y
 
 
-def create_pipeline():
+def create_pipeline(min_samples):
+    k_neighbors = min(min_samples - 1, 5)
+
+    if min_samples <= 2:
+        k_neighbors = 1
+
     numeric_transformer = Pipeline(
         steps=[
             ("imputer", KNNImputer(n_neighbors=2)),
             ("scaler", StandardScaler()),
-            ("poly", PolynomialFeatures(degree=2)),
+            (
+                "poly",
+                PolynomialFeatures(degree=3, interaction_only=True, include_bias=False),
+            ),
         ]
     )
 
@@ -111,30 +121,79 @@ def create_pipeline():
         steps=[("onehot", OneHotEncoder(handle_unknown="ignore"))]
     )
 
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ("num", numeric_transformer, ["age_group", "v144", "v712_groups"]),
-            ("cat", categorical_transformer, ["recode_v131", "sex", "educ_group"]),
-        ]
-    )
-
-    pipeline = ImbPipeline(
+    interaction_transformer = Pipeline(
         steps=[
-            ("preprocessor", preprocessor),
+            ("onehot", OneHotEncoder(handle_unknown="ignore")),
             (
-                "classifier",
-                XGBClassifier(
-                    eval_metric="mlogloss",
-                    use_label_encoder=False,
-                    max_depth=10,
-                    n_estimators=300,
-                    learning_rate=0.05,
-                ),
+                "poly",
+                PolynomialFeatures(degree=2, interaction_only=True, include_bias=False),
             ),
         ]
     )
 
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", numeric_transformer, ["age_group", "v144", "v712_groups"]),
+            ("cat", categorical_transformer, ["recode_v131", "sex", "educ_group"]),
+            (
+                "cat_interact",
+                interaction_transformer,
+                ["recode_v131", "sex", "educ_group"],
+            ),
+        ]
+    )
+
+    smote = BorderlineSMOTE(
+        sampling_strategy="auto", k_neighbors=k_neighbors, random_state=42
+    )
+
+    xgb_classifier = XGBClassifier(
+        eval_metric="mlogloss",
+        use_label_encoder=False,
+        max_depth=2,
+        min_child_weight=10,
+        n_estimators=200,
+        learning_rate=0.01,
+        gamma=0.5,
+        reg_alpha=2,
+        reg_lambda=3,
+        subsample=0.5,
+        colsample_bytree=0.4,
+    )
+
+    calibrated_clf = CalibratedClassifierCV(
+        estimator=xgb_classifier,
+        cv=3,
+        method="sigmoid",
+    )
+
+    pipeline = ImbPipeline(
+        steps=[
+            ("resample", smote),
+            ("preprocessor", preprocessor),
+            ("classifier", calibrated_clf),
+        ]
+    )
+
     return pipeline
+
+
+def merge_small_classes(y, threshold=10):
+    class_counts = y.value_counts()
+    small_classes = class_counts[class_counts < threshold].index
+
+    while len(small_classes) > 0:
+        smallest_class = small_classes[0]
+        if len(small_classes) > 1:
+            next_smallest_class = small_classes[1]
+        else:
+            next_smallest_class = class_counts[class_counts >= threshold].index[0]
+
+        y = y.replace(smallest_class, next_smallest_class)
+        class_counts = y.value_counts()
+        small_classes = class_counts[class_counts < threshold].index
+
+    return y
 
 
 def main():
@@ -147,38 +206,33 @@ def main():
     y_arab = y[X["sector"] == 2]
     y_jewish = y[X["sector"] == 1]
 
-    # Filter out classes with fewer than two samples in each sector
-    vc_arab = y_arab.value_counts()
-    y_arab = y_arab[y_arab.isin(vc_arab[vc_arab >= 2].index)]
-    arab_data = arab_data.loc[y_arab.index]
-
-    vc_jewish = y_jewish.value_counts()
-    y_jewish = y_jewish[y_jewish.isin(vc_jewish[vc_jewish >= 2].index)]
-    jewish_data = jewish_data.loc[y_jewish.index]
+    # Merge small classes in each sector
+    y_arab_merged = merge_small_classes(y_arab, threshold=10)
+    y_jewish_merged = merge_small_classes(y_jewish, threshold=10)
 
     # Encode labels
     encoder_arab = LabelEncoder()
     encoder_jewish = LabelEncoder()
-    encoder_arab.fit(y_arab.unique())
-    encoder_jewish.fit(y_jewish.unique())
+    encoder_arab.fit(y_arab_merged.unique())
+    encoder_jewish.fit(y_jewish_merged.unique())
 
     # Save the encoders
     joblib.dump(encoder_arab, "output/encoder_arab.joblib")
     joblib.dump(encoder_jewish, "output/encoder_jewish.joblib")
 
-    y_arab_encoded = encoder_arab.transform(y_arab)
-    y_jewish_encoded = encoder_jewish.transform(y_jewish)
+    y_arab_encoded = encoder_arab.transform(y_arab_merged)
+    y_jewish_encoded = encoder_jewish.transform(y_jewish_merged)
 
     # Split data into training and testing sets for each sector
     X_arab_train, X_arab_test, y_arab_train, y_arab_test = train_test_split(
-        arab_data,
+        arab_data.loc[y_arab_merged.index],
         y_arab_encoded,
         test_size=0.2,
         random_state=42,
         stratify=y_arab_encoded,
     )
     X_jewish_train, X_jewish_test, y_jewish_train, y_jewish_test = train_test_split(
-        jewish_data,
+        jewish_data.loc[y_jewish_merged.index],
         y_jewish_encoded,
         test_size=0.2,
         random_state=42,
@@ -191,50 +245,21 @@ def main():
     unique_jewish, counts_jewish = np.unique(y_jewish_train, return_counts=True)
     min_class_size_jewish = counts_jewish.min()
 
-    # Set n_neighbors to one less than the number of samples in the smallest class
-    min_k_neighbors = 5  # Set a minimum threshold for k_neighbors
-    smote_arab = BorderlineSMOTE(
-        sampling_strategy="auto",
-        random_state=42,
-        k_neighbors=min(max(1, min_class_size_arab - 1), min_k_neighbors),
-        kind="borderline-1",  # or "borderline-2" based on specific needs
-    )
-    smote_jewish = BorderlineSMOTE(
-        sampling_strategy="auto",
-        random_state=42,
-        k_neighbors=min(max(1, min_class_size_jewish - 1), min_k_neighbors),
-        kind="borderline-1",  # or "borderline-2"
-    )
-
-    # Apply SMOTE
-    X_arab_resampled, y_arab_resampled = smote_arab.fit_resample(
-        X_arab_train, y_arab_train
-    )
-    X_jewish_resampled, y_jewish_resampled = smote_jewish.fit_resample(
-        X_jewish_train, y_jewish_train
-    )
-
     # Create pipelines for each sector
-    pipeline_arab = create_pipeline()
-    pipeline_jewish = create_pipeline()
+    pipeline_arab = create_pipeline(min_class_size_arab)
+    pipeline_jewish = create_pipeline(min_class_size_jewish)
 
-    # Define parameter grid
     param_grid = {
-        "classifier__max_depth": [3, 5, 7, 9, 12, 15],
-        "classifier__min_child_weight": [1, 2, 3, 4, 5, 6, 8, 10],
-        "classifier__learning_rate": [0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.25],
-        "classifier__n_estimators": [50, 100, 150, 200, 250, 300, 350, 400, 450, 500],
-        "classifier__colsample_bytree": [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
-        "classifier__subsample": [0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
-        "classifier__gamma": [0, 0.1, 0.2, 0.3, 0.4, 0.5],
-        "classifier__reg_alpha": [0, 0.1, 0.5, 1, 1.5, 2],
-        "classifier__reg_lambda": [0.5, 1, 1.5, 2, 2.5, 3],
+        "classifier__estimator__max_depth": Integer(3, 20),
+        "classifier__estimator__min_child_weight": Integer(1, 15),
+        "classifier__estimator__learning_rate": Real(0.001, 0.3),
+        "classifier__estimator__n_estimators": Integer(100, 1000),
+        "classifier__estimator__colsample_bytree": Real(0.3, 1.0),
+        "classifier__estimator__subsample": Real(0.5, 1.0),
+        "classifier__estimator__gamma": Real(0, 1.0),
+        "classifier__estimator__reg_alpha": Real(0, 5),
+        "classifier__estimator__reg_lambda": Real(0.5, 5),
     }
-
-    # Setup RandomizedSearchCV for each sector
-    scorer = make_scorer(
-        roc_auc_score, multi_class="ovo", response_method="predict_proba"
-    )
 
     # Determine the smallest class size in the training data using numpy
     unique_arab, counts_arab = np.unique(y_arab_train, return_counts=True)
@@ -242,35 +267,37 @@ def main():
     unique_jewish, counts_jewish = np.unique(y_jewish_train, return_counts=True)
     min_class_size_jewish = counts_jewish.min()
 
-    cv_strategy_arab = StratifiedKFold(n_splits=min_class_size_arab)
-    cv_strategy_jewish = StratifiedKFold(n_splits=min_class_size_jewish)
+    # Increase the number of splits in cross-validation
+    cv_strategy_arab = StratifiedKFold(n_splits=min(min_class_size_arab, 10))
+    cv_strategy_jewish = StratifiedKFold(n_splits=min(min_class_size_jewish, 10))
 
-    search_arab = RandomizedSearchCV(
+    # Define a custom scorer that specifies the multi_class mode
+    roc_auc_scorer = make_scorer(roc_auc_score, needs_proba=True, multi_class="ovo")
+
+    search_arab = BayesSearchCV(
         pipeline_arab,
         param_grid,
-        n_iter=10000,
-        scoring=scorer,
+        n_iter=50,
+        scoring=roc_auc_scorer,
         cv=cv_strategy_arab,
         verbose=3,
-        error_score="raise",
         n_jobs=-1,
         random_state=42,
     )
-    search_jewish = RandomizedSearchCV(
+    search_jewish = BayesSearchCV(
         pipeline_jewish,
         param_grid,
-        n_iter=10000,
-        scoring=scorer,
+        n_iter=50,
+        scoring=roc_auc_scorer,
         cv=cv_strategy_jewish,
         verbose=3,
-        error_score="raise",
         n_jobs=-1,
         random_state=42,
     )
 
     # Train each model using RandomizedSearchCV
-    search_arab.fit(X_arab_resampled, y_arab_resampled)
-    search_jewish.fit(X_jewish_resampled, y_jewish_resampled)
+    search_arab.fit(X_arab_train, y_arab_train)
+    search_jewish.fit(X_jewish_train, y_jewish_train)
 
     # Best models after hyperparameter tuning
     best_model_arab = search_arab.best_estimator_
